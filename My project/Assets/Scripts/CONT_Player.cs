56cc57ed065d0134c5b635b301999fa0432d9f7d
Fernando Nieto Morales <fernando.nietoms@gmail.com>
2026-04-12 13:45:14 -0600

    Update Map game with stamina

--- My project/Assets/Scripts/CONT_Player.cs
+++ My project/Assets/Scripts/CONT_Player.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class CONT_Player : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] Slider staminaBar;
+    [SerializeField] float staminaMin = 3;
+    [SerializeField] float staminaMax = 10;
+    [SerializeField] float stamina;
+    float Stamina { get { return stamina; } set { if (value < staminaMax && value > staminaMin) stamina = value; } }
 
     InputSystem.InputSystem controls;
     public InputSystem.InputSystem Controls { get { return controls; } private set { controls = value; } }
@@ -18,12 +24,17 @@ public class CONT_Player : MonoBehaviour
         Controls.Player.Move.canceled += context => Movement = Vector2.zero;
 
         agent.updateRotation = false;
+
+        agent.speed = stamina;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer(movement);
+        Stamina -= 0.01f;
+        agent.speed = stamina;
+        staminaBar.value = stamina / staminaMax;
     }
 
     void MovePlayer(Vector3 movement)
